a63c07d7a0adf2ad7d049879d61d834ef6f6e6bc
Noah Rosamilia <ivoahivoah@gmail.com>
2017-04-15 15:47:58 -0400

    Fix bounds

--- source/main.c
+++ source/main.c
@@ -3,9 +3,13 @@
 
 #include "place.h"
 
+inline int min (int a, int b) { return a < b ? a : b; }
+inline int max (int a, int b) { return a > b ? a : b; }
+
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
 int cx = 0;
 int cy = 0;
-
 bool update = true;
 
 int main(void) {
@@ -19,10 +23,13 @@ int main(void) {
         int d = 1;
         if (keys) update = true;
         if (keys & ((1 << 0) | (1 << 1))) d = 3;
-        if (keys & (1 << 6) && cy > 0) cy -= d;
-        if (keys & (1 << 7) && cy < 1000 - 160) cy += d;
-        if (keys & (1 << 5) && cx > 0) cx -= d;
-        if (keys & (1 << 4) && cx < 1000 - 240) cx += d;
+        if (keys & (1 << 6)) cy -= d;
+        if (keys & (1 << 7)) cy += d;
+        if (keys & (1 << 5)) cx -= d;
+        if (keys & (1 << 4)) cx += d;
+
+        cx = constrain(cx, 0, 1000 - 240);
+        cy = constrain(cy, 0, 1000 - 160);
 
         if (update) {
             for (int y = 0; y < 160; y++) {
