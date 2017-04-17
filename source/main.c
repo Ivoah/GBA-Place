19425e8b1e4c03d72ae37fa80b98dcbb4d3bae62
Noah Rosamilia <ivoahivoah@gmail.com>
2017-04-16 23:37:45 -0400

    Add partial zoom support

--- source/main.c
+++ source/main.c
@@ -8,6 +8,20 @@
 int cx = 0;
 int cy = 0;
 
+BGAffineSource zoom = {
+    //Center Of Rotation In Original Image (Last 8-Bits Fractional)
+    .x = 0x00007800, // X
+    .y = 0x00005000, // Y
+    // Center Of Rotation On Screen
+    .tX = 0x0078, // X
+    .tY = 0x0050, // Y
+    // Scaling Ratios (Last 8-Bits Fractional)
+    .sX = 0x0100, // X
+    .sY = 0x0100, // Y
+    // Angle Of Rotation ($0000..$FFFF Anti-Clockwise)
+    .theta = 0x0000,
+};
+
 int main(void) {
     irqInit();
     irqEnable(IRQ_VBLANK);
@@ -19,7 +33,7 @@ int main(void) {
     }
 
     while (true) {
-        u8 keys = ~REG_KEYINPUT;
+        u16 keys = ~REG_KEYINPUT;
 
         if (keys) {
             int d = 1;
@@ -28,13 +42,20 @@ int main(void) {
             if (keys & (1 << 7)) cy += d;
             if (keys & (1 << 5)) cx -= d;
             if (keys & (1 << 4)) cx += d;
+            if (keys & (1 << 8)) {zoom.sX -= 4; zoom.sY -= 4;}
+            if (keys & (1 << 9)) {zoom.sX += 4; zoom.sY += 4;}
+            //if (keys & (1 << 8)) zoom.theta += 128;
+            //if (keys & (1 << 9)) zoom.theta -= 128;
 
             cx = constrain(cx, 0, place_width - 240);
             cy = constrain(cy, 0, place_height - 160);
 
             for (int y = 0; y < 160; y++) {
                 dmaCopy(&place[(y + cy)*place_width + cx], (u16*)(VRAM + y*240*2), 240*2);
+                //memcpy((u16*)(VRAM + y*240*2), &place[(y + cy)*place_width + cx], 240*2);
             }
+
+            BgAffineSet(&zoom, (BGAffineDest*)&REG_BG2PA, 1);
         }
 
         VBlankIntrWait();
